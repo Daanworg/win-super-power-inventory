91e10a8e8d217c5c0a9f69b463ab5602774111b9
Daanworg <129817381+Daanworg@users.noreply.github.com>
2025-06-24 09:59:20 +0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 // Your actual Supabase Project URL and Public Key
-const SUPABASE_URL = 'VERCEL_PUBLIC_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'VERCEL_PUBLIC_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://fyaikvhafxodkswnzbtd.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5YWlrdmhhZnhvZGtzd256YnRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MDAxODUsImV4cCI6MjA2NjI3NjE4NX0.LWPp2pVVjdUFc8I2g8qpquk3deGHyvNmYzpMog_wBR0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
